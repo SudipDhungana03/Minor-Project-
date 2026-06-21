9099ab0b624d8de308352c6816ef5ad03aeca82f
SudipDhungana03 <sudipdhungana2063@gmail.com>
2026-06-21 14:36:53 +0545

    feat: implement clean signup and login backend logic

--- backend-django/apps/authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User  # This imports your custom User model
-
-# Register your model using Django's specialized User management interface
-admin.site.register(User, UserAdmin)
